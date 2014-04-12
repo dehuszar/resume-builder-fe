57e339c5b835e31233f43fdaa607607388e1f1f1
Samuel Allen <sda@essentialforms.com>
2014-04-12 18:17:12 -0500

    Attempting integration of scribe-rte

---
+++ app/routes/cover-letter/create.js
@@ -0,0 +1,16 @@
+/* /routes/cover-letter/create.js */
+
+export default Ember.Route.extend({
+	model: function(){
+		// the model for this route is a new empty Ember.Object
+		return Em.Object.create({});
+	},
+
+	// in this case (the create route), we can reuse the coverLetter/edit template
+	// associated with the coverLettersCreateController
+	renderTemplate: function(){
+		this.render('coverLetter.edit', {
+			controller: 'coverLetterCreate'
+		});
+	}
+});
