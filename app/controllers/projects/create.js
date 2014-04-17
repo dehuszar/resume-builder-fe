21b164b99bc8ecd3a8359e6164aee4dc66e2c141
Samuel Allen <sda@essentialforms.com>
2014-04-17 13:39:43 -0500

    local edit changes.  May have conflicts.  Bracing

---
+++ app/controllers/projects/create.js
@@ -0,0 +1,19 @@
+/* /controllers/projects/create.js
+*/
+var ProjectsCreateController = Ember.ObjectController.extend({
+	createForm: 	true,
+	actions: {
+		save: function(){
+			// just before saving, we set the creationDate
+			// this.get('model').set('creationDate', new Date());
+
+			// create a record and save it to the store
+			this.get('model').save();
+
+			// redirects to the user itself
+			// this.transitionToRoute('workhistory', newWorkHistory);
+		}
+	}
+});
+
+export default ProjectsCreateController;
