8041fea18216be83af6c2f32d342ec6c72d6f801
Samuel Allen <sda@essentialforms.com>
2014-04-18 11:02:45 -0500

    Got skills creation, editing, and animation as far as it can go for now.

--- app/views/skills.js
+++ app/views/skills.js
@@ -5,6 +5,17 @@ var SkillsView = Ember.View.extend({
 	SkillItem: Ember.View.extend({
 		attributeBindings: 	['data-state'],
 		tagName: 						'li',
+		
+		skillLeft: null,
+		skillTop: null,
+		skillHeight: null,
+
+		didInsertElement: function() {
+			var pos = this.$().position();
+
+			this.set('skillLeft', pos.left);
+			this.set('skillTop', pos.top);
+		},
 
 		// properties
 		'data-state': function() {
@@ -16,10 +27,17 @@ var SkillsView = Ember.View.extend({
 
 		// actions
 		click: function() {
-			if(this.get('isSelected'))
-				this.set('isSelected', false);
-			else
+			if(this.get('isSelected')) {
+				Ember.run.later(this,function(){
+					this.set('isSelected', false);
+				}, 600);
+			} else {
+				this.$().css({/*"height": this.get('skillHeight'), */"left": this.get('skillLeft'), "top": this.get('skillTop')}).animate({left: "0", top: "0"}, 500);
 				this.set('isSelected', true);
+				var desc = this.$().find('.desc').height();
+				console.log("height: " + (desc + 36));
+				// debugger;
+			}
 		}
 		
 	})
